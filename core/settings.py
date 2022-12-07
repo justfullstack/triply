b13c8d167e3df0897d1022545e3425bca14af90e
Chege CJN <chegecjn@gmail.com>
2022-12-07 12:54:35 +0300

    added DB environment settings

--- core/settings.py
+++ core/settings.py
@@ -296,15 +296,15 @@ DATABASES = {
         }
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL") 
 
-# if not DEBUG:
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             default=DATABASE_URL, 
-#             conn_max_age=1800
-#             ),
-#         }
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL, 
+        conn_max_age=1800
+        ),
+    }
 
 
 #     DATABASE_URL = os.getenv("DATABASE_URL")
